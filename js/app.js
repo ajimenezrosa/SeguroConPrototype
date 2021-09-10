5c410fc4499c508deb4fcb200f326306e35ba380
Jose Alejandro Jimenez Rosa <ja.jimenezrosa@gmail.com>
2021-09-10 10:46:50 -0400

    Primeros pasos y Primer Prototype

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// Constructores
+
+function Seguro(marca, year , tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+
+}
+
+function UI() {
+
+}
+
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear();
+    const min = max -10;
+
+    const selectYear = document.querySelector('#year');
+
+    for( let i = max; i>=min; i--){
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+
+    }
+}
+
+
+// Instanciar UI
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    ui.llenarOpciones();  // llena el select con los Anios
+})
+
