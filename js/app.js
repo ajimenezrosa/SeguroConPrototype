3da16c435441624226d360d8f552086a9e398f3b
Jose Alejandro Jimenez Rosa <ja.jimenezrosa@gmail.com>
2021-09-10 11:07:26 -0400

    Validar Formulario

--- js/app.js
+++ js/app.js
@@ -35,3 +35,34 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones();  // llena el select con los Anios
 })
 
+
+addEventListener();
+function addEventListener() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarseguro);
+}
+
+
+function cotizarseguro(e) {
+  e.preventDefault();
+
+  // leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+    
+    // leer el anio seleccionado
+    const year = document.querySelector('#year').value;
+    console.log(year);
+
+  // leer el tipo de cobertura
+  // este objeto es un radio buttom los readio button se leen en javascript de la siguiente manera
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    if( marca=== '' || year ==='' || tipo === '') {
+        // console.log('No paso la Validacion');
+
+
+    } else {
+        console.log('Si paso la Validacion');
+    }
+
+}
+
